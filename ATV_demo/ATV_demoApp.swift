2ab671aa35108f2e244845969a9dc8d2ca75a095
Ilya Levchenko <ilya@bindimaps.com>
2025-10-21 15:01:11 +1100

    Initial Commit

---
+++ ATV_demo/ATV_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATV_demoApp.swift
+//  ATV_demo
+//
+//  Created by Ilya Levchenko on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ATV_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
